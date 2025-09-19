573b7864cead54195ceb8c2419aaa6b1077023de
alaaSnayel <alaanayel111@gmail.com>
2025-09-19 10:29:40 +0300

    Backend: add JwtTokenValidator and register it in filter chain

---
+++ backend/whatsapp/src/main/java/com/alaa/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.alaa.config;
+
+public class JwtConstant {
+
+  public static final String JWT_HEADER = "Authorization";
+
+  public static final String SECRET_KEY = "yourSecretKeyYourSecretKeyYourSecretKey";
+}
