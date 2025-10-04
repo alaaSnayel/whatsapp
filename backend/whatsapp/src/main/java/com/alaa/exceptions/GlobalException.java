c0f9325dd7c7c7a65813706484ecfa6a3aff095b
alaaSnayel <alaanayel111@gmail.com>
2025-10-04 14:52:51 +0300

    Backend: Add GlobalException handler for centralized error management

--- backend/whatsapp/src/main/java/com/alaa/exceptions/GlobalException.java
+++ backend/whatsapp/src/main/java/com/alaa/exceptions/GlobalException.java
@@ -4,15 +4,41 @@ import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 public class GlobalException {
 
   @ExceptionHandler(UserException.class)
   public ResponseEntity<ErrorDetail> UserExceptionHandler(UserException e, WebRequest req) {
+
+    ErrorDetail err = new ErrorDetail(e.getMessage(),req.getDescription(false), LocalDateTime.now());
+
+    return new ResponseEntity<ErrorDetail>(err, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(MessageException.class)
+  public ResponseEntity<ErrorDetail> MessageExceptionHandler(MessageException e, WebRequest req) {
+
+    ErrorDetail err = new ErrorDetail(e.getMessage(),req.getDescription(false), LocalDateTime.now());
+
+    return new ResponseEntity<ErrorDetail>(err, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<ErrorDetail> MethodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e, WebRequest req) {
+
+    ErrorDetail err = new ErrorDetail(e.getMessage(),req.getDescription(false), LocalDateTime.now());
+
+    return new ResponseEntity<ErrorDetail>(err, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(NoHandlerFoundException.class)
+  public ResponseEntity<ErrorDetail> NoHandlerFoundExceptionHandler(NoHandlerFoundException e, WebRequest req) {
 
     ErrorDetail err = new ErrorDetail(e.getMessage(),req.getDescription(false), LocalDateTime.now());
 
---
+++ backend/whatsapp/src/main/java/com/alaa/exceptions/MessageException.java
@@ -0,0 +1,7 @@
+package com.alaa.exceptions;
+
+public class MessageException extends Exception {
+  public MessageException(String message) {
+    super();
+  }
+}
